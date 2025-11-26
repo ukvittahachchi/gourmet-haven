6d84758d5da5ed8539f10b70fc2a648a9abfd7f2
ukvittahachchi <ukvittahachchi99@gmail.com>
2025-11-26 16:04:50 +0530

    test

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false
-  },
-  server: {
-    host: true
-  }
 })
